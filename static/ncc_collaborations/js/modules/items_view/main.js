5b6b39e7e31bdb0b703b47e8748e108e655f2dc4
Valeriy Zhukow <valeriyzhukow@gmail.com>
2016-02-29 16:13:47 +0200

    закомитил удаленные файлы вручную

--- static/ncc_collaborations/js/modules/items_view/main.js
+++
@@ -1,1 +0,0 @@
-angular.module('nccCollaborations.itemsView', ['ui.bootstrap']);
--- static/ncc_collaborations/js/modules/utilites/preventClickEventDirective.js
+++
@@ -1,8 +0,0 @@
-angular.module('nccCollaborations.itemsView')
-    .directive('preventClickEvent', function() {
-        return function(scope, element) {
-            element.on("click", function(event) {
-                event.stopPropagation();
-            });
-        };
-    });
